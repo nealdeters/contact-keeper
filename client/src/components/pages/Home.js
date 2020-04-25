f3236636d1af15414802f80e44e31877fe78e37d
Neal Deters <nealdeters@gmail.com>
2020-04-25 14:52:23 -0500

    added register and login authentication setup for ui. Connected add contact to api.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Contacts from '../contacts/Contacts'
 import ContactForm from '../contacts/ContactForm'
 import ContactFilter from '../contacts/ContactFilter'
+import AuthContext from '../../context/auth/authContext'
 
 const Home = props => {
+	const authContext = useContext(AuthContext);
+
+	useEffect(() => {
+		authContext.loadUser();
+
+		// es-lint-disable-next-line
+	}, []);
+
 	return (
 		<div className="grid-2">
 			<div>
